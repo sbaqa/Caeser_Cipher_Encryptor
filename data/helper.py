4385120c71e202cf0e0b315d47ce3afc7acc1c95
Sviatoslav Bordovski <sbordovski@gmail.com>
2022-06-02 14:04:47 +0300

    Update helper.py

--- data/helper.py
+++ data/helper.py
@@ -17,3 +17,24 @@ a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8
 """
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+#Encrypt your message
+def encrypt(plain_text, shift_amount):
+  encoded_text = ""
+  for letter in plain_text:
+    alph_position = alphabet.index(letter)
+    shifted_position = alph_position + shift_amount
+    encoded_text += alphabet[shifted_position]
+  print(f"The encoded text is {encoded_text}")
+
+#Decrypt your message
+def decrypt(plain_text, shift_amount):
+  decoded_text = ""
+  for char in plain_text:
+    if char in alphabet:
+      alph_position = alphabet.index(char)
+      shifted_position = alph_position - shift_amount
+      decoded_text += alphabet[shifted_position]
+    else:
+      decoded_text += char
+  print(f"The decoded text is {decoded_text}")
